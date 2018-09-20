a2215012e18e01f970590da43a769145f9ea3161
Andrew Suderman <andrew@sudermanjr.com>
2018-09-20 14:33:01 -0400

    Ingore pyc.  Batch the ingresses now

--- testing/ingress.py
+++ testing/ingress.py
@@ -14,17 +14,7 @@ from reload_time import check_nginx_reload
 
 NS = "ingress-testing"
 baseurl = "test.sudermanjr.hillghost.com"
-
-config.load_kube_config()
-ingress_header = """
-apiVersion: extensions/v1beta1
-kind: Ingress
-metadata:
-  name: test
-  annotations:
-    kubernetes.io/ingress.class: "nginx-ingress"
-spec:
-  rules:"""
+batchSize = 80
 
 def generate_new_hosts_list(ingress_count):
     """Write the hosts file if it does not exist"""
@@ -32,6 +22,11 @@ def generate_new_hosts_list(ingress_count):
         with open("hosts.txt", "w") as file:
             for _ in range(ingress_count):
                 file.write("{}.{}\n".format(uuid.uuid4(), baseurl))
+
+def batch(iterable, n=batchSize):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
 
 def load_hosts_list():
     """grab the list of hosts"""
@@ -47,8 +42,8 @@ def update_ingresses(ingress_count):
     # Do an initial generation of the file
     generate_new_hosts_list(ingress_count)
 
-    # If the new number is lower than the existing, then delete the hosts.txt and then regenerate it.
-    if len(load_hosts_list()) > ingress_count:
+    # If the new number is different than the existing, then delete the hosts.txt and then regenerate it.
+    if len(load_hosts_list()) != ingress_count:
         os.remove("hosts.txt")
         generate_new_hosts_list(ingress_count)
 
@@ -62,37 +57,51 @@ def update_ingresses(ingress_count):
     # Read the hosts into the list again
     hosts = load_hosts_list()
 
-    # Create the ingress definition
-    with open("ingresses.yaml", "w") as file:
-        file.write(ingress_header)
+    ingressCount = 1
+    for hostBatch in batch(hosts):
+        ingress_header = """
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: test-{}
+  annotations:
+    kubernetes.io/ingress.class: "nginx-ingress"
+spec:
+  rules:""".format(ingressCount)
 
-        for host in hosts:
-            ingress_host = """
-      - host: {}
-        http:
-          paths:
-          - backend:
-              serviceName: test-service
-              servicePort: 80
-            path: /""".format(host)
+        # Create the ingress definition
+        with open("ingresses/test-{}.yaml".format(ingressCount), "w") as file:
+            file.write(ingress_header)
 
-            file.write(ingress_host)
+            for host in hostBatch:
+                ingress_host = """
+    - host: {}
+      http:
+        paths:
+        - backend:
+            serviceName: test-service
+            servicePort: 80
+          path: /""".format(host)
 
-    # Deploy the ingresses
-    with open('ingresses.yaml', 'r') as file:
-        ing = yaml.load(file)
-        k8s_beta = client.ExtensionsV1beta1Api()
-        resp = k8s_beta.list_namespaced_ingress(
-            namespace=NS)
-        exists = False
-        for ingress in resp.items:
-            if ingress.metadata.name == 'test':
-                exists = True
+                file.write(ingress_host)
 
-    if exists:
-        resp2 = k8s_beta.replace_namespaced_ingress(body=ing, namespace=NS, name='test')
-    else:
-        resp2 = k8s_beta.create_namespaced_ingress(body=ing, namespace=NS)
+        # Deploy the ingress
+        with open('ingresses/test-{}.yaml'.format(ingressCount), 'r') as file:
+            ing = yaml.load(file)
+            k8s_beta = client.ExtensionsV1beta1Api()
+            resp = k8s_beta.list_namespaced_ingress(
+                namespace=NS)
+            exists = False
+            for ingress in resp.items:
+                if ingress.metadata.name == 'test-{}'.format(ingressCount):
+                    exists = True
+
+        if exists:
+            resp2 = k8s_beta.replace_namespaced_ingress(body=ing, namespace=NS, name='test')
+        else:
+            resp2 = k8s_beta.create_namespaced_ingress(body=ing, namespace=NS)
+
+        ingressCount += 1
 
 if __name__ == "__main__":
-    update_ingresses(100)
+    update_ingresses(4000)
